d9cae85f1e2c7b99d96ebe324bfac2743fe63306
holyantony <holyantony1492@gmail.com>
2012-09-03 16:13:39 +0530

    Subject:Added layouts for all types of report
    Description:
    1. Added popup layouts for report page such as ledger.xml,trial_balance.xml,balance_sheet.xml,project_statement.xml,report_dialog.xml
    Following are the popup options:
    
    1. for ledger
    	a. select account name(drop down)
    	b. date(datepicker)
    	c. narration(checkbox)
    	d. project Name(drop down)
    
    2. for trial balance
    	a. date(date picker)
    	b. Trial Balance Type(drop down)
    
    3. for project statement
    	a. date(date picker)
    	b.  project Name(drop down)
    
    4. for balance sheet
    	a. date(date picker)
    	b. Balance sheet Type(drop down)
    
    5. for income and expenditure
    	a. date(datepicker)
    
    6. for cash flow
    	a. date(datepicker)

--- src/com/example/gkaakash/reportMenu.java
+++ src/com/example/gkaakash/reportMenu.java
@@ -1,21 +1,29 @@
 package com.example.gkaakash;
 
+import java.util.Calendar;
+
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class reportMenu extends ListActivity{
 	//adding report list items
-	String[] reportType = new String[] { "Ledger","Trial Balance","Project Statement","Cash Flow","Balance Sheet","Income and Expenditure" };
+	String[] reportType = new String[] { "Ledger","Trial Balance","Project Statement","Cash Flow","Balance Sheet","Profit and Loss Account" };
 	final Context context = this;
-	
+	AlertDialog dialog;
+
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -37,51 +45,225 @@ public class reportMenu extends ListActivity{
 				//for "Ledger"
 				if(position == 0)
 				{
-					String reporttypeflag  = parent.getItemAtPosition(position).toString();
+					
+					
+					/*String reporttypeflag  = parent.getItemAtPosition(position).toString();
 					Intent intent = new Intent(context, report.class);
 					intent.putExtra("flag", reporttypeflag);
 					// To pass on the value to the next page
-					startActivity(intent);
+					startActivity(intent);*/
+					LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+					View layout = inflater.inflate(R.layout.ledger, (ViewGroup) findViewById(R.id.layout_root));
+					//Building DatepPcker dialog
+					AlertDialog.Builder builder = new AlertDialog.Builder(context);
+					builder.setView(layout);
+					
+					
+					builder.setTitle("");
+					builder.setPositiveButton("Set",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					
+					builder.setNegativeButton("Cancel",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					dialog=builder.create();
+	        		dialog.show();
+					
 				}
 				if(position == 1)
 				{
-					String reporttypeflag  = parent.getItemAtPosition(position).toString();
+					/*String reporttypeflag  = parent.getItemAtPosition(position).toString();
 					Intent intent = new Intent(context, report.class);
 					intent.putExtra("flag", reporttypeflag);
 					// To pass on the value to the next page
-					startActivity(intent);
+					startActivity(intent);*/
+					LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+					View layout = inflater.inflate(R.layout.trial_balance, (ViewGroup) findViewById(R.id.layout_root));
+					//Building DatepPcker dialog
+					AlertDialog.Builder builder = new AlertDialog.Builder(context);
+					builder.setView(layout);
+					builder.setTitle("");
+					
+					builder.setPositiveButton("Set",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					
+					builder.setNegativeButton("Cancel",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					dialog=builder.create();
+	        		dialog.show();
 				}
 				if(position == 2)
 				{
-					String reporttypeflag  = parent.getItemAtPosition(position).toString();
+					/*String reporttypeflag  = parent.getItemAtPosition(position).toString();
 					Intent intent = new Intent(context, report.class);
 					intent.putExtra("flag", reporttypeflag);
 					// To pass on the value to the next page
-					startActivity(intent);
+					startActivity(intent);*/
+					
+					LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+					View layout = inflater.inflate(R.layout.project_statement, (ViewGroup) findViewById(R.id.layout_root));
+					//Building DatepPcker dialog
+					AlertDialog.Builder builder = new AlertDialog.Builder(context);
+					builder.setView(layout);
+					builder.setTitle("");
+					
+				
+					builder.setPositiveButton("Set",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					
+					builder.setNegativeButton("Cancel",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					dialog=builder.create();
+	        		dialog.show();
 				}
 				if(position == 3)
 				{
-					String reporttypeflag  = parent.getItemAtPosition(position).toString();
+					/*String reporttypeflag  = parent.getItemAtPosition(position).toString();
 					Intent intent = new Intent(context, report.class);
 					intent.putExtra("flag", reporttypeflag);
 					// To pass on the value to the next page
-					startActivity(intent);
+					startActivity(intent);*/
+					
+					LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+					View layout = inflater.inflate(R.layout.report_dialog, (ViewGroup) findViewById(R.id.layout_root));
+					//Building DatepPcker dialog
+					AlertDialog.Builder builder = new AlertDialog.Builder(context);
+					builder.setView(layout);
+					
+					
+					 builder.setPositiveButton("Set",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						 
+					 });
+					 builder.setNegativeButton("Cancel",new  DialogInterface.OnClickListener(){
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								// TODO Auto-generated method stub
+								
+							}
+							 
+						 });
+					dialog=builder.create();
+	        		dialog.show();
 				}
 				if(position == 4)
 				{
-					String reporttypeflag  = parent.getItemAtPosition(position).toString();
+					/*String reporttypeflag  = parent.getItemAtPosition(position).toString();
 					Intent intent = new Intent(context, report.class);
 					intent.putExtra("flag", reporttypeflag);
 					// To pass on the value to the next page
-					startActivity(intent);
+					startActivity(intent);*/
+					LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+					View layout = inflater.inflate(R.layout.balance_sheet, (ViewGroup) findViewById(R.id.layout_root));
+					//Building DatepPcker dialog
+					AlertDialog.Builder builder = new AlertDialog.Builder(context);
+					builder.setView(layout);
+					
+					
+					
+					builder.setPositiveButton("Set",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					
+					builder.setNegativeButton("Cancel",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						
+					});
+					dialog=builder.create();
+	        		dialog.show();
 				}
 				if(position == 5)
 				{
-					String reporttypeflag  = parent.getItemAtPosition(position).toString();
+					/*String reporttypeflag  = parent.getItemAtPosition(position).toString();
 					Intent intent = new Intent(context, report.class);
 					intent.putExtra("flag", reporttypeflag);
 					// To pass on the value to the next page
-					startActivity(intent);
+					startActivity(intent);*/
+					
+					
+					LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+					View layout = inflater.inflate(R.layout.report_dialog, (ViewGroup) findViewById(R.id.layout_root));
+					//Building DatepPcker dialog
+					AlertDialog.Builder builder = new AlertDialog.Builder(context);
+					builder.setView(layout);
+					
+					
+					 builder.setPositiveButton("Set",new  DialogInterface.OnClickListener(){
+
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							// TODO Auto-generated method stub
+							
+						}
+						 
+					 });
+					 builder.setNegativeButton("Cancel",new  DialogInterface.OnClickListener(){
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								// TODO Auto-generated method stub
+								
+							}
+							 
+						 });
+					dialog=builder.create();
+	        		dialog.show();
 				}
 				
 				
